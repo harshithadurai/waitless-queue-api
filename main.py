85077129467827accc70ad7a7f3349ae006d17fe
hduraiba <hduraiba@uwaterloo.ca>
2024-03-17 21:25:35 -0400

    testing and random shit

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
-machines = ["treadmill", "row", "cable", "chest_press", "elliptical", "stair_master", "leg_press"]
+machines = ["Treadmill", "Stationarybike", "Ellipticaltrainer", "Rowingmachine", "Smith machine", "Legpressmachine", "Chestpress", "Latpulldownmachine", "Legextensionmachine", "Legcurlmachine", "Seatedcalfraisemachine", "Cablecrossovermachine", "Shoulderpressmachine", "Dumbbellrack", "Adjustablebench", "Abdominalcrunchmachine", "Hipabduction", "Assistedpull-up", "Smithmachine", "Hacksquatmachine"]
 
 machine_queues = {} # Queues for each machine. key=machine_id value=set of users
 
@@ -23,11 +23,14 @@ def lol():
 def join_queue(user_id: str):
 
     if user_id in users:
-        raise HTTPException(status_code=400, detail="User already exists")
+        return {"message": "User {} already exists".format(user_id)}
+        # raise HTTPException(status_code=404, detail="User already exists")
     
+    print(user_id)
     users.add(user_id)
     user_queues[user_id] = []
 
+    printUsers()
     return {"message": "You have successfully initialized user: {}".format(user_id)}
 
 # user_id is removed from set of users
@@ -42,6 +45,7 @@ def join_queue(user_id: str):
     for _, val in machine_queues.items():
         val.remove(user_id)
 
+    printUsers()
     return {"message": "You have successfully removed user: {}".format(user_id)}
 
 # user_id joins queue for machine_id
@@ -49,16 +53,19 @@ def join_queue(user_id: str):
 def join_queue(machine_id: str, user_id: str):
 
     if user_id not in users:
-        raise HTTPException(status_code=400, detail="User does not exist")
+        raise HTTPException(status_code=401, detail="User does not exist")
     if machine_id not in machine_queues:
-        raise HTTPException(status_code=400, detail="Machine not in list")
-    if user_id in user_queues and len(user_queues[user_id]) >= 5:
-        raise HTTPException(status_code=400, detail="You cannot join more than 5 queues.")
-    
-    machine_queues[machine_id].add(user_id)
-    if user_id not in user_queues:
-        user_queues[user_id] = []
-    user_queues[user_id].append(machine_id)
+        raise HTTPException(status_code=402, detail="Machine not in list")
+    # if user_id in user_queues and len(user_queues[user_id]) >= 5:
+    #     raise HTTPException(status_code=403, detail="You cannot join more than 5 queues.")
+
+    if machine_id not in user_queues[user_id]: # if user is alr in queue for machine_id
+        machine_queues[machine_id].add(user_id)
+        if user_id not in user_queues:
+            user_queues[user_id] = []
+        user_queues[user_id].append(machine_id) 
+
+    printUsers()
     return {"message": "You have successfully joined the queue for machine {}".format(machine_id)}
 
 # user_id exits queue for machine_id
@@ -74,6 +81,8 @@ def leave_queue(machine_id: str, user_id: str):
     
     machine_queues[machine_id].remove(user_id)
     user_queues[user_id].remove(machine_id)
+
+    printUsers()
     return {"message": "You have successfully left the queue for machine {}".format(machine_id)}
 
 # returns the number of people in queue for machine_id
@@ -83,6 +92,7 @@ def get_queue_count(machine_id: str):
     if machine_id not in machine_queues:
         raise HTTPException(status_code=404, detail="Machine {} not found".format(machine_id))
     
+    printUsers()
     return {"count": len(machine_queues[machine_id])}
 
 # returns the queues that user_id is currently waiting in
@@ -94,5 +104,12 @@ def get_user_queue_count(user_id: str):
     if user_id not in user_queues:
         return {"count": 0, "queues": []}
     
+    printUsers()
     return {"count": len(user_queues[user_id]), "queues": user_queues[user_id]}
+
+
+def printUsers():
+    for userid in users:
+        print("User:", userid)
+        print(user_queues[userid])
 
